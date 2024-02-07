a72e9b8690eee3946e986b968bd89c60f67d4686
Pawan <134945859+Pawanjhajha@users.noreply.github.com>
2024-02-07 21:29:34 +0530

    create connection with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,49 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {  ConfigModule, ConfigService} from '@nestjs/config';
+import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+ 
+  imports: [ConfigModule.forRoot({
+    envFilePath:[".env",".local.env"],
+    load:configuration,
+    cache:true,
+    isGlobal:true,
+  }),
+ // MongooseModule.forRoot(process.env.DATABASE_DSN),
+MongooseModule.forRootAsync({
+  imports:[ConfigModule],
+useFactory:(configService:ConfigService)=>{
+const envType=configService.get("NODE_ENV");
+
+if(envType==="LOCAL"){
+ 
+return{
+   
+  uri:process.env.DATABASE_DSN
+}
+}
+
+   const username=configService.get("DATABASE_USER");
+   const password=configService.get("DATABASE_PASSWORD");
+ const DATABASE_URL=configService.get("DATABASE_URL");
+ const DATABASE_PORT=configService.get("DATABASE_PORT");
+ const DATABASE_NAME=configService.get("DATABASE_NAME")
+ const url=`${DATABASE_URL}:${DATABASE_PORT}/${DATABASE_NAME}`
+
+ return{
+  uri:url,
+ }
+},
+inject:[ConfigService]
+ })
+ 
+],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+ 
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService=app.get(ConfigService)//get configService
+  const port=configService.get<number>('PORT')
+
+  const prefix=configService.get<string>("API_PREFIX")
+
+   if(prefix){
+     app.setGlobalPrefix(prefix)
+   }
+
+  await app.listen(port || 5000);
 }
 bootstrap();
